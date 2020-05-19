883efb991897dbad8f0e5172caeec5d183010396
Michael Fleming <michael.fleming@nih.gov>
2020-05-19 09:22:54 -0400

    added limited test suite
    added limited test suite

---
+++ Test Suites/Canine_TestSuite_limited.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Canine_TestSuite_limited</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e944574e-e256-4a15-825e-9759428ab06e</testSuiteGuid>
+   <testCaseLink>
+      <guid>763424b6-7b5e-4029-a837-90ae7f8f4aec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Breed/TC01_Canine_Filter_Breed-Akita</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>495784a1-fa51-46f1-b36f-ab074a4d072e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Breed/TC02_Canine_Filter_Breed-AmerStaffd</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9e85382-d674-483d-96f8-e504f93c1427</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Breed/TC09_Canine_Filter_Breed-BorderCol</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b53d352e-75ec-4f8f-8e9b-ccb41b392893</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Breed/TC10_Canine_Filter_Breed-Bouvier</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>765b3304-4985-4d21-bba5-8e5c6d6139ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Diagnosis/TC03_Canine_Filter_Diagnosis-LipMandible</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>295b9202-1740-4753-a87c-b2c9b4c63268</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Diagnosis/TC15_Canine_Filter_Diagnosis-NotSpecified</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d674d76-6f38-434f-a188-4de833131f2d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Gender/TC01_Canine_Filter_Gender-CastratedMale</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a14af720-cb83-4eb6-bff5-32afc6ee2d4e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/Study/TC01_Canine_Filter_Study-COTB</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a6f0d70-5594-4f53-94e6-05e5c9fe1884</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyType/TC01_Canine_Filter_StudyType-ClinicalTrial</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b89dd487-7b60-47a6-9aa6-5954461cf866</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/TC02_Canine_END_TO_END_SelectSingleCaseID</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
